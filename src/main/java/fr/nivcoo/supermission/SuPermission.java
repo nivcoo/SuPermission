6377590b4032f4ed644867ca2f3a422a951d2784
nivcoo <nico64800@live.fr>
2020-11-15 15:33:15 +0100

    first

---
+++ src/main/java/fr/nivcoo/supermission/SuPermission.java
@@ -0,0 +1,26 @@
+package fr.nivcoo.supermission;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import fr.nivcoo.supermission.superiorskyblock.permissions.EssentialsFly;
+
+public class SuPermission extends JavaPlugin {
+
+	private static SuPermission INSTANCE;
+
+	@Override
+	public void onEnable() {
+		INSTANCE = this;
+		Bukkit.getPluginManager().registerEvents(new EssentialsFly(), this);
+	}
+
+	@Override
+	public void onDisable() {
+	}
+
+	public static SuPermission get() {
+		return INSTANCE;
+	}
+
+}
