2814a9e7d353c689cc0abf783b9dbb42c1023646
nivcoo <nico64800@live.fr>
2021-09-19 13:53:18 +0200

    improv. remove warnings

--- src/main/java/fr/nivcoo/supermission/SuPermission.java
+++ src/main/java/fr/nivcoo/supermission/SuPermission.java
@@ -1,28 +1,27 @@
 package fr.nivcoo.supermission;
 
+import fr.nivcoo.supermission.settings.EntitiesStacking;
+import fr.nivcoo.supermission.superiorskyblock.permissions.EssentialsFly;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import fr.nivcoo.supermission.settings.EntitiesStacking;
-import fr.nivcoo.supermission.superiorskyblock.permissions.EssentialsFly;
-
 public class SuPermission extends JavaPlugin {
 
-	private static SuPermission INSTANCE;
+    private static SuPermission INSTANCE;
 
-	@Override
-	public void onEnable() {
-		INSTANCE = this;
-		Bukkit.getPluginManager().registerEvents(new EssentialsFly(), this);
-		Bukkit.getPluginManager().registerEvents(new EntitiesStacking(), this);
-	}
+    @Override
+    public void onEnable() {
+        INSTANCE = this;
+        Bukkit.getPluginManager().registerEvents(new EssentialsFly(), this);
+        Bukkit.getPluginManager().registerEvents(new EntitiesStacking(), this);
+    }
 
-	@Override
-	public void onDisable() {
-	}
+    @Override
+    public void onDisable() {
+    }
 
-	public static SuPermission get() {
-		return INSTANCE;
-	}
+    public static SuPermission get() {
+        return INSTANCE;
+    }
 
 }
--- src/main/java/fr/nivcoo/supermission/settings/EntitiesStacking.java
+++ src/main/java/fr/nivcoo/supermission/settings/EntitiesStacking.java
@@ -1,35 +1,34 @@
 package fr.nivcoo.supermission.settings;
 
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-
 import com.bgsoftware.superiorskyblock.api.SuperiorSkyblockAPI;
 import com.bgsoftware.superiorskyblock.api.events.PluginInitializeEvent;
 import com.bgsoftware.superiorskyblock.api.island.Island;
 import com.bgsoftware.superiorskyblock.api.island.IslandFlag;
 import com.bgsoftware.wildstacker.api.events.EntityStackEvent;
 import com.bgsoftware.wildstacker.api.events.SpawnerStackedEntitySpawnEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 
 public class EntitiesStacking implements Listener {
-	private static IslandFlag ENTITIES_STACKING;
+    private static IslandFlag ENTITIES_STACKING;
 
-	@EventHandler
-	public void init(PluginInitializeEvent e) {
-		IslandFlag.register("ENTITIES_STACKING");
-		ENTITIES_STACKING = IslandFlag.getByName("ENTITIES_STACKING");
-	}
+    @EventHandler
+    public void init(PluginInitializeEvent e) {
+        IslandFlag.register("ENTITIES_STACKING");
+        ENTITIES_STACKING = IslandFlag.getByName("ENTITIES_STACKING");
+    }
 
-	@EventHandler
-	public void onEntityStack(EntityStackEvent e) {
-		Island island = SuperiorSkyblockAPI.getIslandAt(e.getTarget().getLocation());
-		if (island != null && !island.hasSettingsEnabled(ENTITIES_STACKING))
-			e.setCancelled(true);
-	}
+    @EventHandler
+    public void onEntityStack(EntityStackEvent e) {
+        Island island = SuperiorSkyblockAPI.getIslandAt(e.getTarget().getLocation());
+        if (island != null && !island.hasSettingsEnabled(ENTITIES_STACKING))
+            e.setCancelled(true);
+    }
 
-	@EventHandler
-	public void onEntityStackedSpawn(SpawnerStackedEntitySpawnEvent e) {
-		Island island = SuperiorSkyblockAPI.getIslandAt(e.getSpawner().getLocation());
-		if (island != null && !island.hasSettingsEnabled(ENTITIES_STACKING))
-			e.setShouldBeStacked(false);
-	}
+    @EventHandler
+    public void onEntityStackedSpawn(SpawnerStackedEntitySpawnEvent e) {
+        Island island = SuperiorSkyblockAPI.getIslandAt(e.getSpawner().getLocation());
+        if (island != null && !island.hasSettingsEnabled(ENTITIES_STACKING))
+            e.setShouldBeStacked(false);
+    }
 }
